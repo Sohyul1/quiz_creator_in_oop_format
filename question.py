7a71b0c6c1c9c0757480ab5a6e952cac91526294
Aeron <dequitoaeron353@gmail.com>
2025-05-25 17:26:47 +0800

    made a method on determining if the user got the correct answer or not

--- question.py
+++ question.py
@@ -5,3 +5,5 @@ class Question:
         self.option = options
         self.correct_answer = correct_answer
 # Check if the user's answer matches the correct one (case and space insensitive).
+    def is_correct(self, user_answer):
+        return user_answer.lower().strip() == self.correct_answer.lower().strip()
