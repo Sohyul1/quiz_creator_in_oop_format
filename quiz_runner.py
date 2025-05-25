2edf4c127e56d3cd907e83b8a2175ad6170bbf72
Aeron <dequitoaeron353@gmail.com>
2025-05-25 19:15:01 +0800

    imported random and Fore, Style, init from colorama also QuizReader adn Question class from previous files then initialized autoreset of colors and QuizRunner class

--- quiz_runner.py
+++ quiz_runner.py
@@ -1,5 +1,18 @@
 # Import random, Fore, style, init from colorama
+import random
+from colorama import Fore, Style, init
+
 # import QuizReader from quiz_reader and Question from questions
+from quiz_reader_oop_format import QuizReader
+from question import Question
+
 # Initialize autoreset of colors 
+init(autoreset=True)
 # Initialize QuizRunner class
+class QuizRunner:
+    def __init__(self, reader):
+        self.reader = reader
+        self.score = 0
+
+
 # Methhod for asking the questions
