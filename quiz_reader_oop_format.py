a6a6e223765163dff88dbe516fa6fea77b9e7653
Aeron <dequitoaeron353@gmail.com>
2025-05-25 18:51:26 +0800

    imported thhe Question class from thhe question file then initialized the QuizReader class, after that made a method to open adn read the file

--- quiz_reader_oop_format.py
+++ quiz_reader_oop_format.py
@@ -1,4 +1,13 @@
 # Import the Question class from question file
+from question import Question
+
+
 # Make a reader method
+class QuizReader:
+    def __init__(self, filename="quiz_questions.txt"):
+        self.filename = filename
+
 # Initialize the quiz reader
+
+
 # Method for reading question
