0ada6bdfbd218dc2bc187cbc5ed9878591eac926
Aeron <dequitoaeron353@gmail.com>
2025-05-25 17:46:30 +0800

    imported the class Queston from question file then initialized the QuestionCreator class and the parameter filename (str): The name of the file to which questions are saved.

--- quiz_creator_in_oop.py
+++ quiz_creator_in_oop.py
@@ -1,4 +1,12 @@
 # Import the question from the "questions" file
+from question import Question
+
 # Initialize quiz creator
+class QuestionCreator:
+    def __init__(self, filename= "questions.txt"):
+        self.filename = filename
+
 # Make a method for inputting the qetionss, choices and right answer
+
+
 # Method for saving as a text file 
