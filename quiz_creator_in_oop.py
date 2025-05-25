6ef717685518e5e63780c368329bb6b9aae98fbe
Aeron <dequitoaeron353@gmail.com>
2025-05-25 17:53:26 +0800

    made a method that prompts the user to enter a quiz question, four answer choices and i used dictionaries for the choices

--- quiz_creator_in_oop.py
+++ quiz_creator_in_oop.py
@@ -1,4 +1,4 @@
-# Import the question from the "questions" file
+# Import the question from the "question" file
 from question import Question
 
 # Initialize quiz creator
@@ -6,7 +6,14 @@ class QuestionCreator:
     def __init__(self, filename= "questions.txt"):
         self.filename = filename
 
-# Make a method for inputting the qetionss, choices and right answer
-
+# Make a method for inputting the quetions, choices and right answer
+    def create_question(self):
+        text = input("\nEnter any question you want: \n")
+        choices = {
+            'A': input("Letter 'A' value: \n"),  
+            'B': input("Letter 'B' value: \n"),  
+            'C': input("Letter 'C' value: \n"),    
+            'D': input("Letter 'D' value: \n")
+        }
 
 # Method for saving as a text file 
