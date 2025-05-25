09b644258af445cb2938db9f190e6b23629a5d22
Aeron <dequitoaeron353@gmail.com>
2025-05-25 17:23:35 +0800

    Initialized the Question object and made the parameters for the text of questions, options and correct answer

--- question.py
+++ question.py
@@ -1,2 +1,7 @@
 # Initialize a question with its text, answer options, and correct answer
+class Question:
+    def __init__(self, text, options, correct_answer):
+        self.text = text
+        self.option = options
+        self.correct_answer = correct_answer
 # Check if the user's answer matches the correct one (case and space insensitive).
