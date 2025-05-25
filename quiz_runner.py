297729e80d218fe4bc24067785b49b7d16b3fe62
Aeron <dequitoaeron353@gmail.com>
2025-05-25 19:06:55 +0800

    pseudocode

---
+++ quiz_runner.py
@@ -0,0 +1,5 @@
+# Import random, Fore, style, init from colorama
+# import QuizReader from quiz_reader and Question from questions
+# Initialize autoreset of colors 
+# Initialize QuizRunner class
+# Methhod for asking the questions
