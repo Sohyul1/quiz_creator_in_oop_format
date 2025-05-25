af5bb1e57b658d9e36b50670dff1cba5fabc5f4a
Aeron <dequitoaeron353@gmail.com>
2025-05-25 19:00:26 +0800

    copied the way i read files on my previous program then return

--- quiz_reader_oop_format.py
+++ quiz_reader_oop_format.py
@@ -8,5 +8,20 @@ class QuizReader:
 
 # Make a reader method 
     def get_questions(self):
-            with open(self.filename, "r") as file:
-                lines = file.readlines()
+        with open(self.filename, "r") as file:
+            lines = file.readlines()
+        
+        questions_and_choices = []
+        line_count = 0
+        while line_count < len(lines):
+            if lines[line_count].startswith("Question:"):
+                question_text = lines[line_count].strip()
+                choice_a = lines[line_count + 1].strip()
+                choice_b = lines[line_count + 2].strip()
+                choice_c = lines[line_count + 3].strip()
+                choice_d = lines[line_count + 4].strip()
+                correct_answer = lines[line_count + 5].strip(".").replace("Answer:", "").strip()
+                questions_and_choices.append((question_text, [choice_a, choice_b, choice_c, choice_d], correct_answer))
+            line_count += 6
+            
+        return questions_and_choices
