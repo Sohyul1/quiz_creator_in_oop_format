00c548a65446150fcd7df8c3d235a4ebea9aef0e
Aeron <dequitoaeron353@gmail.com>
2025-05-26 13:06:03 +0800

    pseudocode

---
+++ try_quiz_here.py
@@ -0,0 +1,6 @@
+# Import the QuizReader class from the quiz_reader....
+# Import the QuizRunner class from quiz_runner  
+# Check if this script is being run directly (not imported as a module).
+# Load all questions from the file and return them as a list of Question objects
+# Create an object hat is responsible for running the quiz logic
+# Run the quiz
