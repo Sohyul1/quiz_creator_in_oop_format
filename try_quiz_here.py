8c8c9b71dc29a537e4e4df91b7dd1dc528cca8f6
Aeron <dequitoaeron353@gmail.com>
2025-05-26 13:28:39 +0800

    imported eveything i need checedk if this script is being run directly created the object to run the quiz them run

--- try_quiz_here.py
+++ try_quiz_here.py
@@ -1,6 +1,15 @@
 # Import the QuizReader class from the quiz_reader....
+from quiz_reader_oop_format import QuizReader
+
 # Import the QuizRunner class from quiz_runner  
+from quiz_runner import QuizRunner
+
 # Check if this script is being run directly (not imported as a module).
-# Load all questions from the file and return them as a list of Question objects
+if __name__ == "__main__":
+    reader = QuizReader("oop_questions.txt")
+
 # Create an object hat is responsible for running the quiz logic
+    runner = QuizRunner(reader)
+
 # Run the quiz
+    runner.run_quiz()
