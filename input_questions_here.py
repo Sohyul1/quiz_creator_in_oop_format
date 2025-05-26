63c3f965a0a3b81c4f20f778a4bd6370c0211e18
Aeron <dequitoaeron353@gmail.com>
2025-05-26 12:27:10 +0800

    pseudocode

---
+++ input_questions_here.py
@@ -0,0 +1,4 @@
+# Import the QuestionCreator class from quiz_creator_in_oop.py file
+# Check if this script is being run directly (and not imported into another script)
+# Create an instance of the QuestionCreator class, passing the file name "oop_questions.txt"
+# Run the method 
