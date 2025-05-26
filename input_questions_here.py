f463ba02999e4e24c6e9bf8cb4e60bc16076f563
Aeron <dequitoaeron353@gmail.com>
2025-05-26 12:33:18 +0800

    imports the QuizCreator from quiz creator... the checks the script then create an instance of the QuestionCreator class passing the file name of the question storage and then run

--- input_questions_here.py
+++ input_questions_here.py
@@ -1,4 +1,9 @@
 # Import the QuestionCreator class from quiz_creator_in_oop.py file
+from quiz_creator_in_oop import QuestionCreator
+
 # Check if this script is being run directly (and not imported into another script)
-# Create an instance of the QuestionCreator class, passing the file name "oop_questions.txt"
-# Run the method 
+if __name__ == "__main__":
+    # Create an instance of the QuestionCreator class, passing the file name "oop_questions.txt"
+    creator = QuestionCreator("oop_questions.txt")
+    # Run the method 
+    creator.run()
